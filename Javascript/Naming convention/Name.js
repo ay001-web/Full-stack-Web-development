2427d5850327d2458110c94de981ee602031645b
ay001-web <ayushyadav2250@gmail.com>
2025-12-17 22:52:51 +0530

    Name

---
+++ Javascript/Naming convention/Name.js
@@ -0,0 +1,10 @@
+//Naming can include _and $ special characters in the code//
+
+var myName = "Ayush";
+var yourName = "Jack";
+var myvar = "abc";
+var my_name = "Nobita";
+console.log(myName);
+console.log(yourName);
+console.log(myvar);
+console.log(my_name);
