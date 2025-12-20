ad835c563a4928e8c2b88428fecd9450e3ca38b9
ay001-web <ayushyadav2250@gmail.com>
2025-12-20 21:39:02 +0530

    Random Number Generation

--- Javascript/Random number/Random.js
+++ Javascript/Random number/Random.js
@@ -2,5 +2,6 @@ prompt("What is your name?");
 prompt("What is their name?");
 
 var lovescore = Math.random() * 100;
-lovescore = Math.floor(lovescore);
+lovescore = Math.floor(lovescore) +1;
 alert("Your score is " +lovescore + " % . ");
+console.log(lovescore);
