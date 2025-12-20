5e4b63fef6d36c8bdab4b5210cd3c64dc039c2b3
ay001-web <ayushyadav2250@gmail.com>
2025-12-20 21:38:43 +0530

    Control Statement using If Else

---
+++ Javascript/Control Statement/IfElse.js
@@ -0,0 +1,11 @@
+prompt("What is your name");
+prompt("What is their name");
+
+var lovescore = Math.random() * 100;
+lovescore = Math.floor(lovescore) + 1;
+if(lovescore>70) {
+    alert("your lovescore is " +lovescore );
+}
+else {
+    alert("lovescore is " +lovescore);
+}
