fcebd57477c60c0c8302b095e1f3adc5613184cf
ay001-web <ayushyadav2250@gmail.com>
2025-12-26 23:11:52 +0530

    Manipulating Text And Attributes

---
+++ jQuery/Manipulating text and Attributes/text.js
@@ -0,0 +1,14 @@
+/* Manipulating text
+
+$("h1").text("Bye");
+$("button").text("Dont Click me");
+
+*/
+
+
+/*  Manipulating attribute
+
+$("img").attr("src");
+$("a").attr("href","https://www.yahoo.com");
+
+*/
