dd3d3a66a3dd2c6bc12b52faf0c1e3180fe68018
ay001-web <ayushyadav2250@gmail.com>
2025-12-26 23:38:14 +0530

    Adding And Removing Elements

---
+++ jQuery/Adding and removing Elements/element.js
@@ -0,0 +1,8 @@
+$("h1").before("<button>New</button>");
+$("h1").after("<button>New</button>");
+$("h1").prepend("<button>New</button>");
+$("h1").append("<button>New</button>");
+
+//For removing
+
+$("button").remove();
