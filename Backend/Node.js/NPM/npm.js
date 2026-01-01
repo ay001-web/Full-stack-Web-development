a0fc887dd248b384d25093155876d88799e9e73d
ay001-web <ayushyadav2250@gmail.com>
2026-01-01 23:23:27 +0530

    NPM

---
+++ Backend/Node.js/NPM/npm.js
@@ -0,0 +1,9 @@
+//var generateName = require("sillyname");
+//import generateName from "sillyname";
+//var sillyname = generateName();
+//console.log(`My name is ${sillyname}.`);
+
+import {randomSuperhero} from "superheroes";
+const name = randomSuperhero();
+console.log(`I am ${name}.`);
+
